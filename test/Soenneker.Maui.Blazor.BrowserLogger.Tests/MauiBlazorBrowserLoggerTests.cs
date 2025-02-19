2f4683b50ca3db11fccdacb716f856e0dac863a0
Jake Soenneker <jake@soenneker.com>
2025-02-19 08:38:44 -0600

    tests

--- test/Soenneker.Maui.Blazor.BrowserLogger.Tests/MauiBlazorBrowserLoggerTests.cs
+++ test/Soenneker.Maui.Blazor.BrowserLogger.Tests/MauiBlazorBrowserLoggerTests.cs
@@ -1,5 +1,4 @@
-﻿using Soenneker.Maui.Blazor.BrowserLogger.Abstract;
-using Soenneker.Tests.FixturedUnit;
+﻿using Soenneker.Tests.FixturedUnit;
 using Xunit;
 
 namespace Soenneker.Maui.Blazor.BrowserLogger.Tests;
@@ -7,11 +6,9 @@ namespace Soenneker.Maui.Blazor.BrowserLogger.Tests;
 [Collection("Collection")]
 public class MauiBlazorBrowserLoggerTests : FixturedUnitTest
 {
-    private readonly IMauiBlazorBrowserLogger _util;
-
     public MauiBlazorBrowserLoggerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
     {
-        _util = Resolve<IMauiBlazorBrowserLogger>(true);
+
     }
 
     [Fact]
